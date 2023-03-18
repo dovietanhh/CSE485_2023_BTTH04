b265812efa7dd56d1f1cb4737fb011769570d64f
dovietanhh <99164900+dovietanhh@users.noreply.github.com>
2023-03-18 23:10:18 +0700

    nop bai 4

---
+++ l_crud/app/Http/Controllers/StudentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $data = Student::latest()->paginate(5);
+
+        return view('index', compact('data'))->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+}
